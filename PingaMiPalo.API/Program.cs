9eb0f06c663ac467fb0302eda7ab2f137df648d7
yansquinter0 <yansquintero7@gmail.com>
2026-02-15 18:57:31 -0500

    Disable automatic database migrations
    Comment out automatic database migration code.

--- PingaMiPalo.API/Program.cs
+++ PingaMiPalo.API/Program.cs
@@ -78,11 +78,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Apply migrations automatically
+/*
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     dbContext.Database.Migrate();
-}
+}*/
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
